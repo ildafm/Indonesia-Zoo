567840d15de1ebde321592881aa8eecdbfcfda0f
ildafm <ildafm4000@mhs.mdp.ac.id>
2022-05-26 21:07:24 +0700

    Menyelesaikan Persyaratan Tugas
    ->Desain Aplikasi

--- app/src/main/java/com/ildafm/indonesiazoo/ModelKebunBinatang.java
+++ app/src/main/java/com/ildafm/indonesiazoo/ModelKebunBinatang.java
@@ -2,7 +2,7 @@ package com.ildafm.indonesiazoo;
 
 public class ModelKebunBinatang {
 
-    private String nama, detail, foto, lokasi, website, telepon;
+    private String nama, detail, foto, website, telepon;
 
     public String getNama() {
         return nama;
@@ -26,14 +26,6 @@ public class ModelKebunBinatang {
 
     public void setFoto(String foto) {
         this.foto = foto;
-    }
-
-    public String getLokasi() {
-        return lokasi;
-    }
-
-    public void setLokasi(String lokasi) {
-        this.lokasi = lokasi;
     }
 
     public String getWebsite() {
